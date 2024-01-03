14c7e4c597102dc0461d1af4f82127be313d2856
Iyanu <iyanuxn@gmail.com>
2024-01-03 17:09:28 +0100

    Add loading state and document ready event listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
   const [time, setTime] = useState(new Date());
   const [date, setDate] = useState(new Date());
+
   const timeSettings = {
     hour: "numeric",
     minute: "numeric",
@@ -14,7 +16,30 @@ const App = () => {
     month: "long",
     day: "numeric",
   };
-  
+
+  useEffect(() => {
+    const finishLoading = () => {
+      setLoading(false);
+    };
+
+    if (document.readyState === 'complete') {
+      finishLoading();
+    } else {
+      /* The code `document.addEventListener('readystatechange', () => {
+              if (document.readyState === 'complete') {
+                finishLoading();
+              }` is adding an event listener to the document object. It listens for changes in the
+      readyState property of the document. When the readyState becomes 'complete', indicating that
+      the document has finished loading, the `finishLoading()` function is called. This is used to
+      determine when the document has finished loading and set the `loading` state to false. */
+      document.addEventListener('readystatechange', () => {
+        if (document.readyState === 'complete') {
+          finishLoading();
+        }
+      });
+    }
+  }, []);
+
   useEffect(() => {
     const timerID = setTimeout(() => {
       setTime(new Date());
@@ -27,14 +52,14 @@ const App = () => {
 
   return (
     <div className="w-screen h-screen relative">
-      <div className="w-screen absolute flex flex-col mt-28 justify-center items-center">
-        <span className="text-6xl font-semibold">
+      <div className="w-screen absolute flex flex-col mt-44 justify-center items-center font-semibold text-white">
+        <span className="text-[5rem] leading-[6rem]">
           {time.toLocaleTimeString([], timeSettings)}
         </span>
-        <span>{date.toLocaleDateString([], dateSettings)}</span>
+        <span className="text-sm">{date.toLocaleDateString([], dateSettings)}</span>
       </div>
       <img
-        src="https://images.unsplash.com/photo-1490598000245-075175152d25?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="wallpaper"
         className="w-full h-full object-cover"
       />
